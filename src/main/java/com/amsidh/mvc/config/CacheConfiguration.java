71d4fed1efa563775d4150db6e0259cdd6213fe5
amsidhmicroservice <amsidhmicroservice@gmail.com>
2021-12-08 17:30:11 +0530

    Cache timetolive adjusted

--- src/main/java/com/amsidh/mvc/config/CacheConfiguration.java
+++ src/main/java/com/amsidh/mvc/config/CacheConfiguration.java
@@ -1,5 +1,6 @@
 package com.amsidh.mvc.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -9,17 +10,25 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
+import java.time.Duration;
+import java.time.temporal.ChronoUnit;
+
 @Configuration
 @EnableCaching
 public class CacheConfiguration {
 
     public static final String ITEMS_CACHE = "items";
 
+    @Value("${spring.cache.redis.time-to-live:60}")
+    private Long timeToLive;
+
     @Bean
     RedisCacheConfiguration redisCacheConfiguration() {
         return RedisCacheConfiguration.defaultCacheConfig()
                 .disableCachingNullValues()
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                                     .fromSerializer(new GenericJackson2JsonRedisSerializer()))
+                .entryTtl(Duration.ofSeconds(timeToLive));
     }
 
     @Bean
